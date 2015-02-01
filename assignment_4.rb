5a4856a7ccefb7eacf7a3a0c32a9bb7f64ddbf1c
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-01 10:48:32 -0500

    true false math problems

---
+++ assignment_4.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+puts 3 < 4
+puts (3 * 2) < 10
+puts 2 != 3
+puts (1 > 0) && (2 >= 1)
+puts 2 == 2
+puts (14 <= 10) || (2 <= 3)
+puts 10 % 2 == 0
+puts (10 ** 2) == 100
+puts 1000 / 300 < 4
