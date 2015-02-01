762f43009ee5ae9e9dbb560f23c1e8eddb51e6bf
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-01 10:52:18 -0500

    pig latin

---
+++ assignment_14.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+class PigLatin
+  VOWELS =  %w(a e i o u)
+
+  def self.pigatize(passed_text)
+    # Check to see if the first letter is a vowel, if not it's a consonant
+    if PigLatin.starts_with_vowel(passed_text[0])
+      pigatized_text = passed_text + "way"
+    else
+      # Takes first letter, moves it to the end, and adds "ay"
+      pigatized_text = passed_text.slice(1..-1) + "ah" + passed_text.slice(0)
+    end
+
+    return pigatized_text
+  end
+
+  # Check to see if the first letter is a vowel
+  def self.starts_with_vowel(first_letter)
+    VOWELS.include?(first_letter)
+  end
+end
+
+puts "Please enter a word and I will translate to pig latin"
+text = gets.chomp
+puts "Pigatized: #{PigLatin.pigatize(text)}"
