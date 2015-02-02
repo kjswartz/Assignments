20152d58ee3594efc4d8333d80ee0df4440b4488
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-02 16:31:15 -0500

    modified to work with new has output method.

--- Day 5/assignment_15.rb
+++ Day 5/assignment_15.rb
@@ -1,17 +1,15 @@
 #!/usr/bin/ruby
-# array of questions to ask the user
-questions = File.new("questions.txt", "r")
-
+# string of questions stored in q_str variable
+q_str = File.read("questions.txt")
+# q_str split on line break and each line stored in questions array
+questions = q_str.split(/\n/)
 # answers array to store answers from user
 answers = []
 # iterate over each question in order to call and display them
 questions.each { |q| print q; answers << gets.chomp }
-questions.close
 # displays answers
-puts "Howdy #{answers[0].capitalize}!" if answers[0].length > 0
-puts "Your favorite colore is #{answers[1].downcase}." if answers[1].length > 0
-puts "You wear size #{answers[2]} shoe." if answers[2].length > 0
-puts "And as far as hanging out, you said #{answers[3]}." if answers[3].length > 0
-puts "Why didn't you answer any of my questions?" \
-      if answers[0].length == 0 && answers[1].length == 0 && \
-         answers[2].length == 0 && answers[3].length == 0
+# puts the answers into an array with the questions then flattens them together.
+#     Then creates hash with key value pairing.
+results_hash = Hash[*questions.zip(answers).flatten]
+# puts to screen questions and answers
+results_hash.each { |k, v| puts k + v }
