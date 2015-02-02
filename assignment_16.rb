818020ee1e4d6927383d51404dbe16408ba28ae0
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-02 11:44:27 -0500

    added code comments

--- assignment_16.rb
+++ assignment_16.rb
@@ -1,8 +1,9 @@
 require 'net/http'
-
+# pulls txt from web and stores it in response variable.
 uri = URI('http://www.scifiscripts.com/scripts/5thelement.txt')
 response = Net::HTTP.get(uri)
-
+# creates a new file called fifth_element
 script = File.new("fifth_element.txt", "w")
+# writes text stored in response variable to fifth_element.txt file
 script.write(response)
 script.close
