eb2150c09370a6608aa3f1f21720842e7e37095c
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-07 13:01:27 -0500

    changed output to display when loop finishes.

--- assignment_23/test.rb
+++ assignment_23/test.rb
@@ -11,38 +11,37 @@ invoice = gets.chomp
 invoice = Invoice.new
 invoice_loop = ""
 
-while invoice_loop == ""
-  invoice_item = InvoiceItem.new
+  while invoice_loop == ""
+    invoice_item = InvoiceItem.new
 
-  item_hash = {
-    product_name: invoice_item.product_name,
-    sale_price: invoice_item.sale_price,
-    quantity: invoice_item.quantity,
-    tax: invoice_item.tax}
+    item_hash = {
+      product_name: invoice_item.product_name,
+      sale_price: invoice_item.sale_price,
+      quantity: invoice_item.quantity,
+      tax: invoice_item.tax}
 
-  # store my hashed inovice item into invoice array
-  invoice.items << item_hash
+    # store my hashed inovice item into invoice array
+    invoice.items << item_hash
 
-puts "\n\n"
+    # add another item or end
+    print 'Hit ENTER to add another Invoice Item, else enter "n": '
+    invoice_loop = gets.chomp
+      if invoice_loop == "n"
+          # calls invoice method depicting how many items w/out 0 quantity in invoice
+          puts "Number of invoice items: #{invoice.total_items.size}"
 
-  # calls invoice method depicting how many items w/out 0 quantity in invoice
-  puts "Number of invoice items: #{invoice.total_items.size}"
+          # displays product_names of user inputed (non-0) quantity invoice items
+          puts  invoice.output_items
 
-  # displays product_names of user inputed (non-0) quantity invoice items
-  puts  invoice.output_items
+          # displays pre-tax total
+          puts "Pre-tax cost: #{invoice.pre_tax_cost.sum}"
 
-  # displays pre-tax total
-  puts "Pre-tax cost: #{invoice.pre_tax_cost.sum}"
+          # displays total of tax expense
+          puts "Tax cost: #{invoice.tax_cost.sum}"
 
-  # displays total of tax expense
-  puts "Tax cost: #{invoice.tax_cost.sum}"
-
-  # total invoice cost
-  puts "Total owed: #{invoice.total_cost.sum}"
-
-
-  # add another item or end
-  print 'Hit ENTER to add another Invoice Item, else enter "n": '
-  invoice_loop = gets.chomp
-
-end
+          # total invoice cost
+          puts "Total owed: #{invoice.total_cost.sum}"
+        else
+          invoice_loop = ""
+        end
+  end
