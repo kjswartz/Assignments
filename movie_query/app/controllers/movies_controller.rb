7b888959a1744f735f3d84a4856e57f27c7b23c6
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-11 17:06:55 -0500

    deleted gets and puts. set title to top url.

--- movie_query/app/controllers/movies_controller.rb
+++ movie_query/app/controllers/movies_controller.rb
@@ -1,34 +1,20 @@
 class MoviesController < ApplicationController
   def search
+    @title = params["title"]
     # sets my initial query string
     query_url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json"
     api_key = "2pwuxewequbju5pc2ksqfphx"
-    # prompts user for movie to search
-    print "Please enter a movie to search: "
+    # adds movie title to end of QUERY constent and stores raw json
+    uri = URI("#{query_url}?apikey=#{api_key}&q=#{CGI.escape(@title)}")
+    raw_json = Net::HTTP.get(uri)
 
-    # loops through getting user response and searching for titles
-    loop do
-      movie = gets.chomp
-
-      # breaks loop if nothing is entered
-      break if movie == ""
-
-      # adds movie title to end of QUERY constent and stores raw json
-      uri = URI("#{query_url}?apikey=#{api_key}&q=#{CGI.escape(movie)}")
-      raw_json = Net::HTTP.get(uri)
-
-      # returns movies array containing hash key values of supporting information
-      results = JSON.parse(raw_json)
-      movies = results["movies"]
-      @movie_title = movies.first["title"]
-      @movie_synopsis = movies.first["synopsis"]
-      if @movie_synopsis.length < 1
-        @movie_synopsis = "There was no synopsis listed in the first movie returned."
-      end
-
-      # buffer. Lets user search another title.
-      puts "\n"
-      print "Please enter another title to search or pree ENTER to exit: "
+    # returns movies array containing hash key values of supporting information
+    results = JSON.parse(raw_json)
+    movies = results["movies"]
+    @movie_title = movies.first["title"]
+    @movie_synopsis = movies.first["synopsis"]
+    if @movie_synopsis.length < 1
+      @movie_synopsis = "There was no synopsis listed in the first movie returned."
     end
   end
 end
