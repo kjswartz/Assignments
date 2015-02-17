053d6f7ddb512bc427359885a2582dc55736d9eb
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-17 15:02:40 -0500

    added annotate gem.

--- assignment33_jobs/app/models/job.rb
+++ assignment33_jobs/app/models/job.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id          :integer          not null, primary key
+#  company     :string
+#  location    :string
+#  title       :string
+#  description :text
+#  duties      :text
+#  experience  :text
+#  education   :text
+#  posted      :date
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Job < ActiveRecord::Base
 end
--- assignment33_jobs/test/models/job_test.rb
+++ assignment33_jobs/test/models/job_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id          :integer          not null, primary key
+#  company     :string
+#  location    :string
+#  title       :string
+#  description :text
+#  duties      :text
+#  experience  :text
+#  education   :text
+#  posted      :date
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
